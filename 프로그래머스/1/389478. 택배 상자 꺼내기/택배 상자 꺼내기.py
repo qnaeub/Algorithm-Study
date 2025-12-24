b04027e214aba1d5b6d7834d4220e0b015db008a
윱 <105096696+qnaeub@users.noreply.github.com>
2025-12-24 15:35:20 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.05 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,7 +1,6 @@
 def solution(n, w, num):
     # 상자 번호의 인덱스
-    r_index = 0
-    c_index = 0
+    r_index, c_index = 0, 0
     
     # 행 개수 설정
     r = (n + w - 1) // w
@@ -10,39 +9,24 @@ def solution(n, w, num):
     matrix = [[0] * w for _ in range(r)]
 
     # 상자 쌓기
-    isReverse = True
     k = 0
-    for i in range(r - 1, -1, -1):
-        isReverse = not isReverse
-        # 정방향 쌓기
-        if isReverse == False:
-            for j in range(w):
-                k += 1
-                if k > n:
-                    break
-                matrix[i][j] = k
-                if k == num:
-                    r_index = i
-                    c_index = j
-        # 역방향 쌓기
-        else:
-            for j in range(w - 1, -1, -1):
-                k += 1
-                if k > n:
-                    break
-                matrix[i][j] = k
-                if k == num:
-                    r_index = i
-                    c_index = j
+    for i in range(r):
+        for j in range(w):
+            k += 1
+            if k > n: break
+            
+            real_j = j if i % 2 == 0 else (w - 1) - j
+            
+            matrix[i][real_j] = k
+            if k == num:
+                r_index, c_index = i, real_j
 
     # 꺼내야 하는 상자 개수 세기
     answer = 0
-    for i in range(r_index + 1):
-        if matrix[i][c_index] == 0:
-            continue
-        elif i > r_index:
-            break
-        else:
+    for i in range(r_index, r, 1):
+        if matrix[i][c_index] != 0:
             answer += 1
+        else:
+            break
 
     return answer
