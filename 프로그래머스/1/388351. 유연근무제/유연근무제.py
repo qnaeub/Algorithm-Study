e97a849d5d1bed522dcbe3f50300a51c942c194d
윱 <105096696+qnaeub@users.noreply.github.com>
2025-12-24 10:44:18 +0900

    [level 1] Title: 유연근무제, Time: 1.88 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -4,30 +4,22 @@ def solution(schedules, timelogs, startday):
 
     # 한 사람씩 지각 확인
     for i in range(n):
-        # 출근 희망 시각 -> 출근 가능 시각으로 조정
-        schedules[i] += 10
-        if schedules[i] % 100 > 59:
-            schedules[i] += 40
-            
-        today = startday - 1
+        # (i+1)번째 직원의 정상 출근 마감 시각 설정
+        deadline = schedules[i] + 10
+        if deadline % 100 >= 60:
+            deadline += 40
 
         for j in range(7):
             # 주말인 경우 지각 체크 X
-            today += 1
-            if today > 7:
-                today = 1
-            elif (today == 6) | (today == 7):
+            today = (startday + j - 1) % 7 + 1
+            if today >= 6:
                 continue
 
-            # 지각자 수 체크
-            if schedules[i] < timelogs[i][j]:
+            # (i+1)번째 직원이 지각한 경우, 지각 직원 수 1 증가
+            if deadline < timelogs[i][j]:
                 late += 1
                 break
-            else:
-                j += 1
 
-        i += 1
-
-    # 전체 인원에서 지각자 수 제외
+    # 전체 직원 수에서 지각 직원 수 제외
     answer = n - late
     return answer
