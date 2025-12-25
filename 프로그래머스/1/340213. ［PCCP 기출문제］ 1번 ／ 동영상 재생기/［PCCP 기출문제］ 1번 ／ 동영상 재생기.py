177c1827028f9f0a6d59eb99f0e466d06dcee189
윱 <105096696+qnaeub@users.noreply.github.com>
2025-12-25 11:51:42 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.04 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,37 +1,30 @@
-def ms(t):
-    # 숫자로 변환
+def time_to_seconds(t):
+    # 초 단위로 변환
     m, s = t.split(":")
-    return int(m) * 100 + int(s)
+    return int(m) * 60 + int(s)
+
+def seconds_to_time(s):
+    # 00:00 형식으로 변환
+    m, s = divmod(s, 60)
+    return f"{m:02d}:{s:02d}"
 
 def solution(video_len, pos, op_start, op_end, commands):
-    # 숫자로 변환
-    video_len_ms = ms(video_len)
-    pos_ms = ms(pos)
-    op_start_ms = ms(op_start)
-    op_end_ms = ms(op_end)
+    # 초 단위로 변환
+    video_len_s = time_to_seconds(video_len)
+    pos_s = time_to_seconds(pos)
+    op_start_s = time_to_seconds(op_start)
+    op_end_s = time_to_seconds(op_end)
     
     # 오프닝 구간일 경우, 오프닝 끝나는 위치로 이동
-    if op_start_ms <= pos_ms < op_end_ms: pos_ms = op_end_ms
+    if op_start_s <= pos_s < op_end_s: pos_s = op_end_s
     
     for c in commands:
         # prev: 10초 전으로 이동, next: 10초 후로 이동
-        if c == "prev":
-            pos_ms -= 10
-            if pos_ms % 100 >= 60:
-                pos_ms -= 40
-        else:
-            pos_ms += 10
-            if pos_ms % 100 >= 60:
-                pos_ms += 40
-
         # 동영상 범위 밖인 경우, 영상의 처음 또는 마지막 위치로 이동
-        if pos_ms < 0:
-            pos_ms = 0
-        elif pos_ms > video_len_ms:
-            pos_ms = video_len_ms
-            
+        pos_s = max(0, pos_s - 10) if c == "prev" else min(video_len_s, pos_s + 10)
+
         # 오프닝 구간일 경우, 오프닝 끝나는 위치로 이동
-        if op_start_ms <= pos_ms < op_end_ms: pos_ms = op_end_ms
+        if op_start_s <= pos_s < op_end_s: pos_s = op_end_s
     
     # 00:00 형식으로 반환
-    return f"{str(pos_ms//100).zfill(2)}:{str(pos_ms%100).zfill(2)}"
+    return seconds_to_time(pos_s)
