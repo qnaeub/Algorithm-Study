1715b24f347170a2ec5b6de4dc9ea1cecc871a93
윱 <105096696+qnaeub@users.noreply.github.com>
2025-12-29 16:51:51 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 9.04 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,13 +1,20 @@
 def canPlace(park, mats):
+    r, c = len(park), len(park[0])
+    
     for m in mats:
-        for i in range(len(park)):
-            for j in range(len(park[0])):
-                if i+m <= len(park) and j+m <= len(park[0]) and all(park[k][l] == "-1" for k in range(i, i+m) for l in range(j, j+m)):
-                                return m
+        # 돗자리 길이가 공원 길이보다 더 길면 건너뛰기
+        if m > r or m > c:
+            continue
+
+        for i in range(r - m + 1):
+            for j in range(c - m + 1):
+                # 빈 공간 만나면 돗자리 깔 수 있는지 확인
+                if park[i][j] == "-1":
+                    if all(park[k][l] == "-1" for k in range(i, i+m) for l in range(j, j+m)):
+                        return m
+
     return -1
 
 def solution(mats, park):
-    mats = sorted(mats, reverse=True)
-    answer = canPlace(park, mats)
-
-    return answer
+    # 돗자리 길이 긴 순서대로 정렬
+    return canPlace(park, sorted(mats, reverse=True))
