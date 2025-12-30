35d6f618e8668f17452abf795b816bd2b5b7b133
윱 <105096696+qnaeub@users.noreply.github.com>
2025-12-30 13:18:33 +0900

    [level 1] Title: 과일 장수, Time: 74.22 ms, Memory: 20.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,9 +1,3 @@
 def solution(k, m, score):
-    answer, i = 0, 0
     score.sort(reverse=True)
-    
-    while i < len(score) - (len(score) % m):
-        answer += score[i + m - 1] * m
-        i += m
-        
-    return answer
+    return sum(score[m-1::m]) * m
