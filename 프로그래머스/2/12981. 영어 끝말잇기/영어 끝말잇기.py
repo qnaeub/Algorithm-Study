65bbee4ba20a743637878df0a74642113d431fc3
윱 <105096696+qnaeub@users.noreply.github.com>
2025-12-30 16:01:49 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,13 +1,16 @@
 def solution(n, words):
-    # 첫 번째 단어가 한 글자인 경우
-    if len(words[0]) == 1:
-        return [1, 1]
+    used_words = {words[0]}
+    last_word = words[0]
     
-    # 끝말잇기 규칙 확인
-    for i in range(1, len(words)):
-        for j in range(i):
-            if (words[j] == words[i]) or (len(words[i]) == 1) or (words[i-1].strip()[-1] != words[i].strip()[0]):
-                return [i % n + 1, i // n + 1]
+    for i, word in enumerate(words[1:], start=1):
+        # 끝말잇기 규칙 확인
+        if last_word[-1] != word[0] or word in used_words:
+            player_no = i % n + 1
+            turn_count = i // n + 1
+            return [player_no, turn_count]
+        
+        used_words.add(word)
+        last_word = word
     
     # 탈락자가 생기지 않은 경우
     return [0, 0]
