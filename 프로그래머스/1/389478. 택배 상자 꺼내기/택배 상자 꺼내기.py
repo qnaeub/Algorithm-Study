843e76df29d5639275130a62e8e1e454cb5a1dd8
윱 <105096696+qnaeub@users.noreply.github.com>
2025-12-30 12:01:18 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,33 +1,15 @@
 def solution(n, w, num):
-    # 상자 번호의 인덱스
-    r_index = (num - 1) // w
-    c_index = (num - 1) % w
-    if r_index % 2 != 0:
-        c_index = (w - 1) - c_index
-    
-    # 행 개수 설정
-    r = (n + w - 1) // w
-
-    # 0으로 채운 행렬 생성
-    matrix = [[0] * w for _ in range(r)]
-
-    # 상자 쌓기
-    k = 0
-    for i in range(r):
-        for j in range(w):
-            k += 1
-            if k > n: break
-            
-            real_j = j if i % 2 == 0 else (w - 1) - j
-            
-            matrix[i][real_j] = k
+    # 정방향 + 역방향 = 2w
+    m1 = num % (2 * w)
+    m2 = (2 * w + 1 - m1) % (2 * w)
+    offset = (m2 - m1) % (2 * w)
 
     # 꺼내야 하는 상자 개수 세기
     answer = 0
-    for i in range(r_index, r, 1):
-        if matrix[i][c_index] != 0:
-            answer += 1
-        else:
-            break
+    # num과 같은 방향 상자 개수 세기
+    answer += len(range(num, n + 1, 2 * w))
+    # num과 다른 방향 상자 개수 세기
+    if offset > 0:
+        answer += len(range(num + offset, n + 1, 2 * w))
 
     return answer
