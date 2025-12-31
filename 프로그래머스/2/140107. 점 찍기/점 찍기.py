0f2e0d3b1f9dfa1d23d981125a2320ad7b02b09b
윱 <105096696+qnaeub@users.noreply.github.com>
2025-12-31 14:51:20 +0900

    [level 2] Title: 점 찍기, Time: 683.96 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -0,0 +1,10 @@
+import math
+
+def solution(k, d):
+    answer = 0
+    
+    for i in range(0, d+1, k):
+        j = math.trunc(math.sqrt(d**2 - i**2))
+        answer += j // k + 1
+
+    return answer
